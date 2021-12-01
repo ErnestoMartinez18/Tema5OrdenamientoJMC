30e2766f4008ea3d77b80f7831a5312c266f9d87
ernes <ernes@192.168.155.9>
2021-11-30 19:27:03 -0600

    Primer commit del proyecto de Ordenamiento

---
+++ src/datos/DatosOrdenados.java
@@ -0,0 +1,25 @@
+
+package datos;
+
+
+public class DatosOrdenados {
+    private int [] valores; 
+
+    public DatosOrdenados(int[] valores) {
+        this.valores = valores;
+    }
+    
+    public int getSize(){
+        return valores.length; 
+    }
+    
+    @Override
+    public String toString(){
+        String cad = "Datos ordenados: ";
+        for (int x : valores){
+            cad += x + ", ";
+        }
+        return cad;
+    }
+    
+}

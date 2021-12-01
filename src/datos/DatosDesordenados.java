7d81123c141fae4b1564dcd853094ad87e797e3f
ernes <ernes@192.168.155.9>
2021-11-30 19:42:17 -0600

    Segundo commit, se agrego el metodo burbuja y quicksort

--- src/datos/DatosDesordenados.java
+++ src/datos/DatosDesordenados.java
@@ -1,193 +1,168 @@
 package datos;
 
-
 public class DatosDesordenados {
-    // Atributos
-    private int[] valores; 
-    private int limiteSuperior; 
-    private int size; 
 
-    public DatosDesordenados(int limite, int size) {
-        this.valores = new int[size];
-        this.limiteSuperior = limite;
-        this.size = size;
+    int i, j, temporal, pasadas;
+
+    public DatosDesordenados() {
+        this.i = 0;
+        this.j = 0;
+        this.temporal = 0;
     }
-    
-    public void agregarDato(int valor){
-        valores[size]= valor;
-        size++;
-    }
-    
-    public int getDato(int posicion){
-        return valores[posicion];
-    }
-    
-    public void generarValores(){
-        for (int i=0; i < valores.length; i++){
-            double aleatorio = Math.random() * limiteSuperior;
-            valores[i] = (int) aleatorio + 1;
-        }
-    }
-    
-    public int getSize(){
-        return size; 
-    }
-    
-    public int getCapacidad(){
-        return valores.length;
-    }
-    
-    public int getLimiteSuperior(){
-        return limiteSuperior;
-    }
-    
-    
-    public int[] getValores(){
-        return valores;
-    } 
-    
-    public int[] getCopiaValores(){
-        int [] copia = new int[size];
-        System.arraycopy(valores, 0, copia, 0, size);
-        return copia;
-    }
-    
-    
-   
-     public DatosOrdenados algoritmoSort(){
-        
-        // obtener una copia de los datos desordenados
-        int [] arreglo = this.getCopiaValores();
-        
-        // agregar algoritmo de ordenación
-        
-        // devolver los datos ordenados
-        return new DatosOrdenados(arreglo);
-    }
-    
-    public DatosOrdenados bubbleSort(){
-        int [] arreglo = this.getCopiaValores();
-        int n = arreglo.length;
-        int cont = 0;
-        int ci = 0;
-        for (int i=0; i < n-1; i++){
-            for (int j=0; j < n-i-1; j++){
-                cont++;
-                if (arreglo[j] > arreglo[j+1]){
-                    // intercambiar: swap
-                    ci++;
-                    int temporal = arreglo[j];
-                    arreglo[j] = arreglo[j+1];
-                    arreglo[j+1] = temporal;
+
+    //Metodo burbuja version 1
+    public String burbuja(int[] arreglo) {
+        pasadas = 0;
+        for (i = 0; i < arreglo.length; i++) {
+            for (j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[i] > arreglo[j]) {
+                    temporal = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temporal;
                 }
+                pasadas++;
             }
         }
-        System.out.println("BS-comparaciones = " + cont);
-        System.out.println("BS-intercambios = " + ci);
-        return new DatosOrdenados(arreglo);
+        System.out.println("Termino en " + pasadas + " Pasadas");
+        return mostrarArreglo(arreglo);
     }
-    
-       
-    
-    public DatosOrdenados insertSort(){
-         // obtener una copia de los datos desordenados
-        int [] a = this.getCopiaValores();
-        
-	int i, j, aux; 
-	int cont=0; 
-	for ( i = 1; i < a.length; i++){
-		j = i;
-		aux = a[i];
-		while (j >0 && aux < a[j-1]){
-                   cont++;
-                   a[j] = a[j-1];
-                   j--; 
-		}
-		a[j] = aux; 
-	}
-        System.out.println("IS-comparaciones = " + cont);
-	// devolver los datos ordenados
-        return new DatosOrdenados(a);
-    }
-    
-    public DatosOrdenados mergeSort(){
-    int [] x = this.getCopiaValores();
-	// Aquí va la llamada al método mergesort( arreglo, l, r)
-	this.mergesort( x, 0, x.length - 1);
-        System.out.println("MS-comparaciones = " + cm);
-	return new DatosOrdenados(x);
-    } // fin del método ordenacionMerge
-    
-   private void mergesort (int v[], int l, int r) {
-		 int m = (r+l)/2; 
-		 if (r > l) { 
-                     cm++;
-			 mergesort (v, l, m);
-			 mergesort (v, m+1, r); 
-			 merge (v, l, m, r); 
-		 }
-	}
-    static int cm=0;
-   private void merge (int v[], int l, int m, int r)
-	{
-	//Encuentra el tamaño de los sub-vectores para unirlos.
-	  int n1 = m - l + 1;
-	  int n2 = r - m;
 
-	  //Vectores temporales.
-	  int arrIzq[] = new int [n1];
-	  int arrDer[] = new int [n2];
-
-	  //Copia los datos a los arreglos temporales.
-	  for (int i=0; i < n1; i++) {
-              cm++;
-		arrIzq[i] = v[l+i];
-	  }
-	  for (int j=0; j < n2; j++) {
-              cm++;
-		arrDer[j] = v[m + j + 1];
-	  }
-	  // Combinación (mezcla) de los dos arreglos:
-	  int i = 0, j = 0;
-	  int k = l;
-	  while (i < n1 && j < n2) {//Ordenamiento.
-                cm++;
-		if (arrIzq[i] <= arrDer[j]) {
-			v[k] = arrIzq[i];
-			i++;
-		} else {
-			v[k] = arrDer[j];
-			j++;
-		}
-		k++;
-	  }//Fin del while.
-
-	  /* Si quedan elementos por ordenar */
-	  //Copiar los elementos restantes de arrIzq[].
-	  while (i < n1) {
-              cm++;
-	    v[k] = arrIzq[i];
-	    i++;
-	    k++;
-	  }
-	  //Copiar los elementos restantes de arrDer[].
-	  while (j < n2) {
-              cm++;
-	    v[k] = arrDer[j];
-	    j++;
-	    k++;
-	  }
-	}
-    
-    @Override
-    public String toString(){
-        String cad = "Datos DESORDENADOS: ";
-        for (int x : valores){
-            cad += x + ", ";
+    //Metodo burbuja version 2
+    public void burbija2(int[] arreglo) {
+        pasadas = 0;
+        for (i = 1; i < arreglo.length; i++) {
+            for (j = 0; j < arreglo.length - 1; j++) {
+                if (arreglo[j] > arreglo[j + 1]) {
+                    temporal = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temporal;
+                }
+                pasadas++;
+            }
         }
-        return cad;
+        System.out.println("Termino en " + pasadas + " Pasadas");
     }
     
+    public String burbujaDesendente(int[] arreglo){
+        pasadas=0;
+        for(i=1;i<arreglo.length;i++){
+            for(j=0;j<arreglo.length-1;j++){
+                if(arreglo[j]<arreglo[j+1]){
+                    temporal=arreglo[j];
+                    arreglo[j]=arreglo[j+1];
+                    arreglo[j+1]=temporal;
+                }
+                pasadas++;
+            }
+        }
+        System.out.println("Termino en "+pasadas+" Pasadas");
+        return mostrarArreglo(arreglo);
+}
+
+    //Metodo Radix
+   
     
+     public String quick(int[] arreglo, int primero,int ultimo){
+      
+        int i,j,pivote,auxiliar;
+        
+        i=primero;
+        j=ultimo;
+        
+        pivote=arreglo[(primero+ultimo)/2];
+        System.out.println("El pivote es: "+pivote);
+        
+        do{
+            while(arreglo[i]<pivote){
+                i++;
+            }
+            while(arreglo[j]>pivote){
+                j--;
+            }
+            //Aqui hacemos el intercambio
+            if(i<=j){
+                auxiliar=arreglo[i];
+                arreglo[i]=arreglo[j];
+                arreglo[j]=auxiliar;
+                i++;
+                j--;
+            } 
+        }while(i<=j);
+        if(primero<j){
+            quick(arreglo,primero,j);
+        }
+        if(i<ultimo){
+            quick(arreglo,i,ultimo);
+        }
+        return mostrarArreglo(arreglo);
+    }
+     
+     public String quickDesendente(int[] arreglo, int primero,int ultimo){
+      
+        int i,j,pivote,auxiliar;
+        
+        i=primero;
+        j=ultimo;
+        
+        pivote=arreglo[(primero+ultimo)/2];
+        System.out.println("El pivote es: "+pivote);
+        
+        do{
+            while(arreglo[i]>pivote){
+                i++;
+            }
+            while(arreglo[j]<pivote){
+                j--;
+            }
+            //Aqui hacemos el intercambio
+            if(i<=j){
+                auxiliar=arreglo[i];
+                arreglo[i]=arreglo[j];
+                arreglo[j]=auxiliar;
+                i++;
+                j--;
+            } 
+        }while(i<=j);
+        if(primero<j){
+            quickDesendente(arreglo,primero,j);
+        }
+        if(i<ultimo){
+            quickDesendente(arreglo,i,ultimo);
+        }
+        return mostrarArreglo(arreglo);
+    }
+
+     
     
+    //Metodo para Insersion
+    public void insersion(int[] arreglo, int n){
+        int i,j,auxiliar;
+        for(i=1;i<n;i++){
+            auxiliar=arreglo[i];
+            j=i-1;
+            while(j>=0 && arreglo[j]>auxiliar){
+                arreglo[j+1]=arreglo[j];
+                j=j-1;
+            }
+            arreglo[j+1]=auxiliar;
+        }
+        System.out.println("Arreglo Ordenado por Insercion");
+        mostrarArreglo(arreglo);
+    }
+    
+    //Metodo para Shell
+   
+    
+
+    public String mostrarArreglo(int[] arreglo) {
+        int k;
+        String cadena = "";
+        for (k = 0; k < arreglo.length; k++) {
+            System.out.print("[" + arreglo[k] + "]" + "\n");
+            cadena = cadena + (k + 1) + "----->" + "[" + arreglo[k] + "]" + "\n";
+        }
+        System.out.println("");
+        return cadena;
+    }
+
 }
--- src/programas/TestOrdenamiento.java
+++ src/programas/TestOrdenamiento.java
@@ -21,20 +21,20 @@ public class TestOrdenamiento {
         s = Integer.parseInt(JOptionPane.showInputDialog(
                 "¿Cuál es el valor máximo?"));
         
-        m = new DatosDesordenados(s, n);
-        m.generarValores();
-        System.out.println("" + m);
+        //m = new DatosDesordenados(s, n);
+        //m.generarValores();
+       // System.out.println("" + m);
         
         t1 = System.nanoTime();   // start
-            DatosOrdenados mob = m.bubbleSort();
+            //DatosOrdenados mob = m.bubbleSort();
             t2 = System.nanoTime();   // stop
             t= t2 - t1;
-            System.out.println("n="+ n + " t=" + t + " ns. Burbuja=" + mob);
+            //System.out.println("n="+ n + " t=" + t + " ns. Burbuja=" + mob);
             t1 = System.nanoTime();   // start
-            DatosOrdenados moi = m.insertSort();
+            //DatosOrdenados moi = m.insertSort();
             t2 = System.nanoTime();   // stop
             t = t2 - t1;
-            System.out.println("n="+ n + " t=" + t + " ns. Inserción" + moi);
+           // System.out.println("n="+ n + " t=" + t + " ns. Inserción" + moi);
     }
     
 }
